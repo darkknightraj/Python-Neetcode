3a02c5844bacdef7a01037e7942bcd2efbd7c41d
darkknightraj <rajankumarm30@gmail.com>
2025-04-19 11:29:12 +0530

    valid anagram using hashset leetcode 247

---
+++ Valid_anagram.py
@@ -0,0 +1,16 @@
+def validAnagram(s,t):
+    if len(s) != len(t):
+        return False
+    countS,countT ={},{}
+    for i in range(len(s)):
+        countS[s[i]] = 1+ countS.get(s[i],0)
+        countT[t[i]] = 1+ countT.get(t[i],0)
+    for c in countS:
+        if countS[c] != countT.get(c,0):
+            return False
+    return True
+s="anagram"
+t = "nagrama"
+result=validAnagram(s,t)
+print(result) #Output = True
+
